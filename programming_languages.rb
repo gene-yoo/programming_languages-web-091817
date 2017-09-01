acf90e769c71ac60d880d639c005a912a58986f0
gene-yoo <gene.y.yoo@gmail.com>
2017-09-01 17:59:13 -0400

    Lab complete.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,14 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = languages[:oo].merge(languages[:functional])
+  new_hash.each do |language, properties|
+    new_hash[language][:style] = []
+  end
+
+  languages.each do |style, examples|
+    examples.each do |language, properties|
+      new_hash[language][:style] << style
+    end
+  end
+
+  new_hash
 end
